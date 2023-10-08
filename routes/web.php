d298fc2f30e70c184e210ac54b713a12eab01edd
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-08 21:17:12 +0600

    use fallback route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+require  __DIR__ . '/task-list-app.php';
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+/**
+ * If no registered route match to the incoming
+ * HTTP request This down below fallback function will work
+*/
+Route::fallback(function () {
+    return 'If No route is found, this route will execute';
 });
